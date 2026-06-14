40aa5687843bf5b117d37efe8e769c457c694c93
Eduardo Johanns Michalski <eduardo160403@edu.unipar.br>
2026-06-14 18:47:28 -0300

    commit final

--- app/src/main/java/com/example/trabalhovenda/telas/PedidoTela.kt
+++ app/src/main/java/com/example/trabalhovenda/telas/PedidoTela.kt
@@ -5,15 +5,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.filled.Search
